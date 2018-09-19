370448ce83fa86da0bfefcaaf3959d4f603f3c7a
danpilgrim <d.daniel.pilgrim@gmail.com>
2018-09-19 03:25:49 -0700

    initial commit

---
+++ helloteam.py
@@ -0,0 +1,11 @@
+print("Hello team, thanks for the pull request")
+
+print("To make a personal branch for your task, run $ git branch nameOfTask")
+print("To change to your branch, run $ git checkout nameOfTask")
+print("To view available branches, run $ git branch")
+
+print("Say you want to add changes to your branch, run $ git add --all")
+print("To ommit and add a message, run $ git commit -m \"initial commit\" ")
+print("Say you want to add changes to your local branch, run $ git push")
+
+
---
+++ pushtorepo.py
@@ -0,0 +1,4 @@
+print("Hello team, now its time to push to your remote branch")
+
+
+print("Once your branch is merged, run $ git -d nameOfTask")
