ff43dbf032807a298b405ab03dc9743ab1430cc8
danpilgrim <d.daniel.pilgrim@gmail.com>
2018-09-19 03:38:56 -0700

    changes to blah

--- pushtorepo.py
+++ pushtorepo.py
@@ -3,7 +3,7 @@ print("********************************************")
 print("Hello team, now its time to push to your remote branch")
 
 print("Say you want to add changes to your remote branch on Github, run $ git push --set-upstream origin nameOfTask\n")
-print("Now, to create a pull request, visit https://github.com/danpilgrim/openar-courseme.git and select your branch under \"Branch Master\")
+print("Now, to create a pull request, visit the Github and select your branch under \"Branch Master\")
 
 print("Hit \"Create Merge request\". Include notes about what you want merged, make sure its descriptive! Then, hit \"Create Merge request\" again")
 print("Submit the pull request, and the git moderator should approve your changes to be merged.\n")
