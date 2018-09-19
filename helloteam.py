409be7c7a5eca8f61fe962a4171cbb408b16e21a
danpilgrim <d.daniel.pilgrim@gmail.com>
2018-09-19 03:36:28 -0700

    changes to blah

--- helloteam.py
+++ helloteam.py
@@ -1,11 +1,13 @@
-print("Hello team, thanks for the pull request")
+print("********************************************")
 
+print("Hello team, thanks for running this introduction\n")
 print("To make a personal branch for your task, run $ git branch nameOfTask")
 print("To change to your branch, run $ git checkout nameOfTask")
-print("To view available branches, run $ git branch")
-
+print("To view available branches, run $ git branch\n")
 print("Say you want to add changes to your branch, run $ git add --all")
 print("To ommit and add a message, run $ git commit -m \"initial commit\" ")
-print("Say you want to add changes to your local branch, run $ git push")
+print("Say you want to add changes to your local branch, run $ git push\n")
+print("Repeat this process after you add your own print statements to this program, then run \"pushtorepo.py\"")
+print("********************************************")
 
 
