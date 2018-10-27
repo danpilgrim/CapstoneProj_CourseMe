5f37e02c1a573bb86472c472e94120094d1a64f4
Sam Robertson <srobertson726@gmail.com>
2018-10-27 14:26:44 -0700

    Added firebase config file
    Added file to link firebase project to app.

--- courseme/ios/courseme/AppDelegate.m
+++ courseme/ios/courseme/AppDelegate.m
@@ -10,6 +10,8 @@
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
 
+#import <Firebase.h>
+
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
@@ -17,7 +19,7 @@
   NSURL *jsCodeLocation;
 
   jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
-
+  [FIRApp configure];
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
                                                       moduleName:@"courseme"
                                                initialProperties:nil
