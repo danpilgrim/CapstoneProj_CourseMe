c2a1c830f788a26e4dcf87034c05c5f60ed5047e
Sam Robertson <srobertson726@gmail.com>
2019-01-11 10:46:41 -0700

    Class Data Tool
    Writing tool to generate JSON from Anthony's text files.

---
+++ Tools/ClassDataGenerator/ClassDataGenerator/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ClassDataGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class CSEClass
+    {
+        public string ID { get; set; }
+    }
+}
